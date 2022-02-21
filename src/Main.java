f8b0a62a0e1b8496214c9dd557b70fa3ed1c6e8a
sasa1978 <Maximus.78@mail.ru>
2022-02-21 13:27:25 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+
+        List<Integer> resultList = new ArrayList<>();
+        for (int x: intList) {
+            if (x > 0 && x % 2 == 0) resultList.add(x);
+        }
+
+        Collections.sort(resultList);
+
+        for (int x: resultList) {
+            System.out.println(x);
+        }
+    }
+}
